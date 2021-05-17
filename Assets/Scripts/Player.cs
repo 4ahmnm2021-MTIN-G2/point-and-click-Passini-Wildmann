7c3c2e8543ab9abfdc7f9663ea85e3542966cfa5
LeonhardPassini <leonhard.passini03@htl-salzburg.ac.at>
2021-05-17 09:20:34 +0200

    update graphics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    public Vector3 mPosition;
+
+    public Vector3 playerPos;
+
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        playerPos = this.transform.position;
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            mPosition = Input.mousePosition;
+            
+            Ray ray = Camera.main.ScreenPointToRay(mPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                playerPos = hit.point;
+        }
+        
+            
+            this.transform.position = Vector3.MoveTowards(this.transform.position, playerPos, speed);
+    }
+}
